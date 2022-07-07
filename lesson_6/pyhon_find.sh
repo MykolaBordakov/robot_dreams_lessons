d0401d5cff515ad683050f38d1aa62a96cdf3e36
MykolaBordakov <m.m.bordakov@gmail.com>
2022-07-07 16:02:24 +0300

    Subject: Third partb of the lesson. Add py sercher. Problem Solution Note

---
+++ lesson_6/pyhon_find.sh
@@ -0,0 +1,3 @@
+touch /home/mykola_nemo/lesson_6/py_find.txt
+touch /home/mykola_nemo/lesson_6/py_find_errors.txt
+find / -name "*.py" 2> /home/mykola_nemo/lesson_6/py_find_errors.txt 1> /home/mykola_nemo/lesson_6/py_find.txt
