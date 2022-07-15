9f5bdf3aad898a1d4f4c038647af2c0fb5a2a213
MykolaBordakov <m.m.bordakov@gmail.com>
2022-07-15 14:06:23 +0300

    Subject: Add Kill to enda of the if/ Problem Solution Note

--- lesson_7/status.sh
+++ lesson_7/status.sh
@@ -3,8 +3,8 @@ servstat=$(service apache2 status)
 if [[ $servstat == *"apache2 is running"* ]]; then
   echo "process is running"
   pid=$(ps aux | grep apache2 | grep root | grep -v grep | awk '{print $2}')
-  kill -15 "$pid"
   name=$(ps aux | grep apache2 | grep root | grep -v grep | awk '{print $11}')
+  kill -15 "$pid"
 echo "Process $name $pid stopped"
 else echo "process is not running"
 fi
