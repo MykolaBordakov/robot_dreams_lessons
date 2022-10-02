4837c57ffd0b09c7d3b8f73a007e473f3ba3ef97
MykolaBordakov <m.m.bordakov@gmail.com>
2022-10-02 18:34:40 +0300

    Subject: Instance aws Problem Solution Note

---
+++ Final_proj/AWS_instance/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "pppp"
+  secret_key = "lolk"
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = "ami-017fecd1353bcc96e"
+  instance_type          = "t2.micro"
+  key_name               = "Teraform-key"
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tags = {
+    Name = "Core_instance"
+  }
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
+  ]
+}
